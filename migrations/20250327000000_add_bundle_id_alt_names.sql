6db5e365ec7e84dc7881f18cb77c001856ef74b2
Aljóna Đukić <github@alena.red>
2026-03-27 21:37:19 +0500

    db: 🗃️ flatten migrations

--- migrations/20250327000000_add_bundle_id_alt_names.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE services ADD COLUMN bundle_id TEXT UNIQUE;
-ALTER TABLE services ADD COLUMN alternative_names TEXT[] NOT NULL DEFAULT '{}';
-
-CREATE INDEX idx_services_bundle ON services(bundle_id);
-CREATE INDEX idx_services_alt_names ON services USING gin (alternative_names);
